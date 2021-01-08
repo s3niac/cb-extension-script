c0a7affef353dc1345903866767b69fb9d57a1f1
s3niac <77159785+s3niac@users.noreply.github.com>
2021-01-08 22:42:53 +0100

    Initial commit

---
+++ chaturbate-pattern-tip.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         chaturbate-pattern-tip
+// @namespace    s3niac/cb-extension
+// @version      0.2
+// @description  pattern tipping extension
+// @author       s3niac
+// @match        https://*.chaturbate.com/*
+// @match        https://testbed.cb.dev/*
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.2/dist/cb-extension/runtime.0e49e2b53282f40c8925.js
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.2/dist/cb-extension/polyfills.8c151b8375e767ff858f.js
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.2/dist/cb-extension/main.7446d9b6fa24ec45d789.js
+// @resource     STYLE_CSS https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.2/dist/cb-extension/styles.5a4ce67b4380534c1e2b.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const my_css = GM_getResourceText('STYLE_CSS');
+    GM_addStyle(my_css);
+})();

d977fc38db63ee4cb1115e7235271530e3f3707f
s3niac <77159785+s3niac@users.noreply.github.com>
2021-01-09 01:30:41 +0100

    Update chaturbate-pattern-tip.user.js

--- chaturbate-pattern-tip.user.js
+++ chaturbate-pattern-tip.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         chaturbate-pattern-tip
 // @namespace    s3niac/cb-extension
-// @version      0.2.3
+// @version      0.2.5
 // @description  pattern tipping extension
 // @author       s3niac
 // @match        https://*.chaturbate.com/*
 // @match        https://testbed.cb.dev/*
-// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.3/dist/cb-extension/runtime.0e49e2b53282f40c8925.js
-// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.3/dist/cb-extension/polyfills.8c151b8375e767ff858f.js
-// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.3/dist/cb-extension/main.0fd501cd6ef7c1b9f873.js
-// @resource     STYLE_CSS https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.3/dist/cb-extension/styles.5a4ce67b4380534c1e2b.css
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.5/dist/cb-extension/runtime.0e49e2b53282f40c8925.js
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.5/dist/cb-extension/polyfills.8c151b8375e767ff858f.js
+// @require      https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.5/dist/cb-extension/main.0e63ebdb97abad40573e.js
+// @resource     STYLE_CSS https://cdn.jsdelivr.net/gh/s3niac/cb-extension@0.2.5/dist/cb-extension/styles.5a4ce67b4380534c1e2b.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
